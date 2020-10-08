98e8fd5d087f1e783e720f86ff7dea5db541a936
Clément Seijido <clement@seijido.fr>
2020-10-08 10:08:47 +0200

    Creation of basic commands

--- main.py
+++ main.py
@@ -1,16 +1,78 @@
 # main.py
 import os
+import random
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN') #TOKEN = "NzYzNDE2NDI1ODQyODY4MjI2.X33Y8w.oD0xhHO4owXBvP3fiwl1ztqtrhs"
+GUILD = os.getenv('DISCORD_GUILD') #GUILD = "La crème de la crème"
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f"{client.user} has connected to Discord!")
+    guild = discord.utils.get(bot.guilds, name=GUILD)
+    print(
+        f'{bot.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
 
-client.run(TOKEN)
+@bot.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+
+@bot.command(name='ping', help='Respond pong')
+async def ping(ctx):
+    response = "pong"
+    await ctx.send(response)
+
+
+@bot.command(name='99', help='Respond a B99 quote')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+
+@bot.command(name='roll_dice', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
+
+@bot.command(name='green', help='Send a tree')
+@commands.has_role('Design4green')
+async def green(ctx):
+    response = '🌲'
+    await ctx.send(response)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('Nope, t\'as pas le droit :P')
+    else:
+        await ctx.send('C\'est pas faux')
+
+
+
+bot.run(TOKEN)
