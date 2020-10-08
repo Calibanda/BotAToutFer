824a0067b28644635ef7b6d8d93d2ef3e2caf0e4
Clément Seijido <clement@seijido.fr>
2020-10-08 15:24:21 +0200

    Add tea command. Add logging system. Improve code clarity

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
 # main.py
 import os
 import random
+import logging
+import datetime
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+SCRIPT_DIR, SCRIPT_FILENAME = os.path.split(os.path.abspath(__file__))
 
-bot = commands.Bot(command_prefix='!')
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("DISCORD_GUILD")
+
+LOG_FILENAME = datetime.datetime.now().strftime("%Y_%m_%d") + ".log"
+LOG_FILE_PATH = os.path.join(SCRIPT_DIR, "logs", LOG_FILENAME)
+
+logger = logging.getLogger("discord")
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename=LOG_FILE_PATH, encoding="utf-8", mode="a")
+handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(name)s: %(message)s"))
+logger.addHandler(handler)
+
+
+
+bot_description = "BotAToutFer, le bot qui fait tout, même le café !"
+
+bot = commands.Bot(command_prefix="!", description=bot_description)
 
 
 @bot.event
 async def on_ready():
+    """When the bot is connected to the guild, print guild informations"""
     guild = discord.utils.get(bot.guilds, name=GUILD)
-    print(
-        f'{bot.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+    print(f"{bot.user} is connected to the following guild:\n{guild.name} (id: {guild.id})")
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    """When a command error occures displays the reason in the gild chat"""
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send("Nope, t'as pas le droit :P")
+    else:
+        await ctx.send("https://tenor.com/view/of-its-not-false-its-true-agree-gif-16911578")
 
 
 #@bot.event
@@ -31,52 +55,49 @@ async def on_ready():
 #            raise
 
 
-@bot.command(name='ping', help='Respond pong')
+@bot.command(name="ping", help="Responds pong.")
 async def ping(ctx):
     response = "pong"
     await ctx.send(response)
 
 
-@bot.command(name='99', help='Respond a B99 quote.')
+@bot.command(name="99", help="Responds a B99 quote.")
 async def nine_nine(ctx):
     brooklyn_99_quotes = [
-        'I\'m the human form of the :100: emoji.',
-        'Bingpot!',
-        'Cool. Cool cool cool cool cool cool cool, no doubt no doubt no doubt no doubt.',
+        "I'm the human form of the :100: emoji.",
+        "Bingpot!",
+        "Cool. Cool cool cool cool cool cool cool, no doubt no doubt no doubt no doubt.",
     ]
 
     response = random.choice(brooklyn_99_quotes)
     await ctx.send(response)
 
 
-@bot.command(name='roll_dice', help='Simulates rolling dice.')
+@bot.command(name="roll_dice", help="Simulates rolling dice.")
 async def roll(ctx, number_of_dice: int, number_of_sides: int):
     dice = [
         str(random.choice(range(1, number_of_sides + 1)))
         for _ in range(number_of_dice)
     ]
-    await ctx.send(', '.join(dice))
+    await ctx.send(", ".join(dice))
 
 
-@bot.command(name='green', help='Send a tree.')
-@commands.has_role('Design4green')
+@bot.command(name="green", help="Sends a tree.")
+@commands.has_role("Design4green")
 async def green(ctx):
-    response = ':evergreen_tree:'
+    response = ":evergreen_tree:"
     await ctx.send(response)
 
 
-@bot.command(name='coffee', help='Send a coffee.')
+@bot.command(name="coffee", help="Send a coffee.")
 async def green(ctx):
-    response = ':coffee:'
+    response = ":coffee:"
     await ctx.send(response)
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.errors.CheckFailure):
-        await ctx.send('Nope, t\'as pas le droit :P')
-    else:
-        await ctx.send('https://tenor.com/view/of-its-not-false-its-true-agree-gif-16911578')
+@bot.command(name="tea", help="Send a tea.")
+async def green(ctx):
+    response = ":tea:"
+    await ctx.send(response)
 
 
 bot.run(TOKEN)
