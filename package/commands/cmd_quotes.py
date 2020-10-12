a79d491ffcc21bae1b5204e374432e4503881fbd
Clément Seijido <clement@seijido.fr>
2020-10-12 22:29:19 +0200

    Add 3 B99 quotes. Invert channel test

--- package/commands/cmd_quotes.py
+++ package/commands/cmd_quotes.py
@@ -1,5 +1,5 @@
 # quotes command for BotAToutFer
-import random
+import secrets
 
 from discord.ext import commands
 
@@ -11,31 +11,38 @@ class Quotes(commands.Cog):
 
     @commands.command(name="99", help="Répond une réplique de B99")
     async def nine_nine(self, ctx):
-        if str(ctx.channel.id) == self.bot_channel_id:
-            brooklyn_99_quotes = [
-                "I'm the human form of the :100: emoji.",
-                "Bingpot!",
-                "Cool. Cool cool cool cool cool cool cool, no doubt no doubt no doubt no doubt.",
-                "Every time someone steps up and says who they are, the world becomes a better, more interesting place.",
-                "Haha, the angel puked!",
-                "- I'm not a thug, I'm police.\n- Ok, then name one law.\n- Don't kill people?\n- It's on me. I set the bar too low...",
-                "https://tenor.com/VdlE.gif",
-                "YOU PUNK!",
-                
-            ]
+        if str(ctx.channel.id) != self.bot_channel_id:
+            return
+        
+        brooklyn_99_quotes = [
+            "I'm the human form of the :100: emoji.",
+            "Bingpot!",
+            "Cool. Cool cool cool cool cool cool cool, no doubt no doubt no doubt no doubt.",
+            "Every time someone steps up and says who they are, the world becomes a better, more interesting place.",
+            "Haha, the angel puked!",
+            "- I'm not a thug, I'm police.\n- Ok, then name one law.\n- Don't kill people?\n- It's on me. I set the bar too low...",
+            "https://tenor.com/VdlE.gif",
+            "YOU PUNK!",
+            "Clearly, the pineapple IS the slut.",
+            "It's time to sauce up!",
+            "Oh, damn",
+            
+        ]
 
-            response = random.choice(brooklyn_99_quotes)
-            await ctx.send(response)
+        response = secrets.choice(brooklyn_99_quotes)
+        await ctx.send(response)
 
     @commands.command(name="good_place", help="Répond une réplique de TGP")
     async def good_place(self, ctx):
-        if str(ctx.channel.id) == self.bot_channel_id:
-            the_good_place_quotes = [
-                "Everything is fine",
-                "I'm telling you, Molotov cocktails work. Anytime I had a problem and I threw a Molotov cocktail, boom! Right away, I had a different problem.",
-                "Not a girl",
-                
-            ]
+        if str(ctx.channel.id) != self.bot_channel_id:
+            return
+        
+        the_good_place_quotes = [
+            "Everything is fine",
+            "I'm telling you, Molotov cocktails work. Anytime I had a problem and I threw a Molotov cocktail, boom! Right away, I had a different problem.",
+            "Not a girl",
+            
+        ]
 
-            response = random.choice(the_good_place_quotes)
-            await ctx.send(response)
+        response = secrets.choice(the_good_place_quotes)
+        await ctx.send(response)
