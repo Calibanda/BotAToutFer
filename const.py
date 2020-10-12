a48af0e9e250150f0ed9c711ba7ed7118e44b9be
Clément Seijido <clement@seijido.fr>
2020-10-12 22:31:38 +0200

    Add 1 curse word. Add Scrabble constants

--- const.py
+++ const.py
@@ -26,6 +26,7 @@ CURSE_LIST = [
     {"curse_word": "merde", "traduction": "merle"},
     {"curse_word": "putain", "traduction": "mutin"},
     {"curse_word": "connard", "traduction": "canard"},
+    {"curse_word": "pute", "traduction": "pull"},
 
     {"curse_word": "fuck", "traduction": "fork"},
     {"curse_word": "shit", "traduction": "shirt"},
@@ -36,3 +37,9 @@ CURSE_LIST = [
     {"curse_word": "dick", "traduction": "deck"},
 
 ]
+
+SCRABBLE_DICTIONARY_PATH = os.path.join(SCRIPT_DIR, "package", "ODS7.txt")
+
+FRENCH_SCRABBLE_VALUES = {"A": 1, "B": 3, "C": 3, "D": 2, "E": 1, "F": 4, "G": 2, "H": 4, "I": 1, "J": 8,
+                          "K": 10, "L": 1, "M": 2, "N": 1, "O": 1, "P": 3, "Q": 8, "R": 1, "S": 1, "T": 1,
+                          "U": 1, "V": 4, "W": 10, "X": 10, "Y": 10, "Z": 10}
