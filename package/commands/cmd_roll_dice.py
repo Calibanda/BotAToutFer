3ebda730f2d21eaf5d5e0eb38aa3aa515b487573
Clément Seijido <clement@seijido.fr>
2020-10-21 09:28:25 +0200

    Change dice command name anc correct a confusion between number of dices and number of sides

--- package/commands/cmd_roll_dice.py
+++ package/commands/cmd_roll_dice.py
@@ -8,11 +8,11 @@ class Roll_dice(commands.Cog):
         self.bot = bot
         self._last_member = None
 
-    @commands.command(name="roll_dice", help="Simule un lancer de dés au format xDx")
+    @commands.command(name="dice", help="Simule un lancer de dés au format xDx")
     async def roll(self, ctx, dice: str):
         """Rolls a dice in xDx format."""
         try:
-            number_of_sides, number_of_dice = map(int, dice.lower().split('d'))
+            number_of_dice, number_of_sides = map(int, dice.lower().split('d'))
             response = ", ".join(str(random.randint(1, number_of_sides)) for r in range(number_of_dice))
             await ctx.send(response)
         except Exception:
