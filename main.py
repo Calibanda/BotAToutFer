693e2226ae170b8c0ef69c0789e7b23b5537feac
Clément Seijido <clement@seijido.fr>
2020-10-23 18:58:44 +0200

    Move "possible" and "echec" detectors before the curse word detector. Fix curse word detector

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ async def on_message(message):
         response = f"Salut *{i_am}*, moi c'est le {bot.user.mention}"
         await message.channel.send(response)
 
-    if any(re.match(r"(?<![:\w*:])" + curse_dict["curse_word"], message.content.lower()) + " " for curse_dict in const.CURSE_LIST):
+    if any(re.match(r"(?<![:\w*:])" + curse_dict["curse_word"], message.content.lower() + " ") for curse_dict in const.CURSE_LIST):
         response = f"{message.author.mention} : {message.content} "
         for curse_dict in const.CURSE_LIST:
             response = re.sub(r"(?<![:\w*:])" + curse_dict["curse_word"], "*" + curse_dict["traduction"] + "*", response, flags=re.IGNORECASE)
