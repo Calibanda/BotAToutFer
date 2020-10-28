d3ac570b9e76aec39bd3e416468e8a7d242d1369
Clément Seijido <clement@seijido.fr>
2020-10-28 18:17:35 +0100

    Refactoring and change logic. Bot now reacts on exacts words in the user message rather than comparing substrings with the user message

--- package/on_message_jokes.py
+++ package/on_message_jokes.py
@@ -5,36 +5,34 @@ import discord
 import const
 
 
-async def on_message_jokes(bot, message):
-
+async def joke_je_suis(bot, message):
     if any(je_suis in message.content.lower() for je_suis in const.LIST_JE_SUIS):
-        regex_je_suis = "(" + ")|(".join(const.LIST_JE_SUIS) + ")"
+        regex_je_suis = "(" + ")|(".join(const.LIST_JE_SUIS) + ")" # "(je suis)|(j'suis)|(chuis) ..."
         i_am = re.split(regex_je_suis, message.content, 1, flags=re.IGNORECASE)[-1]
         response = f"Salut *{i_am}*, moi c'est le {bot.user.mention}"
         await message.channel.send(response)
 
-    if "echec" in message.content.lower() or "échec" in message.content.lower():
+
+async def joke_echec(message, words_in_message):
+    if [ word for word in words_in_message if word in {"échec", "echec"} ]: # If common words between words in the original message and "échec" or "echec"
         response = "https://tenor.com/bq4o5.gif"
         await message.channel.send(response)
 
-    if "possible" in message.content.lower():
+
+async def joke_possible(message, words_in_message):
+    if [ word for word in words_in_message if word in {"possible", "impossible"} ]: # If common words between words in the original message and "possible" or "impossible"
         response = "https://tenor.com/XiKZ.gif"
         await message.channel.send(response)
-    
-    if "aled" in message.content.lower():
+
+
+async def joke_aled(message, words_in_message):
+    if "aled" in words_in_message: # If the word "aled" in the original message
         response ="https://assets.classicfm.com/2017/23/aled-jones-new-1497019550-editorial-long-form-1.png"
         await message.channel.send(response)
-    
-    if any(entry["word"].split(" ")[0].lower() in message.content.lower() for entry in const.DICO_MARSEILLAIS):
-        present_words = [ entry for entry in const.DICO_MARSEILLAIS if entry["word"].split(" ")[0].lower() in message.content.lower() ]
-        response = f"Hey <@{const.ANTOINE_TAG}>"
-        for entry in present_words:
-            word = entry["word"]
-            description = entry["description"]
-            response += f", \"{word}\" ça veut dire {description}"
-        await message.channel.send(response)
 
-    if any(re.search(r"(?<!\:)" + curse_dict["curse_word"], message.content, flags=re.IGNORECASE) for curse_dict in const.CURSE_LIST):
+
+async def cursed_words(message, words_in_message):
+    if [ word for word in words_in_message for curse_dict in const.CURSE_LIST if word == curse_dict["curse_word"] ]: # If any curse word in the original message
         response = message.content
         for curse_dict in const.CURSE_LIST:
             response = re.sub(r"(?<!\:)" + curse_dict["curse_word"], "*" + curse_dict["traduction"] + "*", response, 0, flags=re.IGNORECASE)
@@ -42,3 +40,27 @@ async def on_message_jokes(bot, message):
         response = f"{message.author.mention} : {response}"
         await message.delete()
         await message.channel.send(response)
+
+
+async def marseillais_word(message, words_in_message):
+    present_words = [ word for word in words_in_message for entry in const.DICO_MARSEILLAIS if word == entry["word"].split(" ")[0].lower() ] # Catch all the marseillais words
+    if present_words: # If any marseillais word in the original message
+        response = f"Hey <@{const.ANTOINE_TAG}>, "
+        for entry in present_words:
+            word = entry["word"]
+            description = entry["description"]
+            response += f"\"{word}\" ça veut dire {description}\n"
+        response = response[:-1]
+        await message.channel.send(response)
+
+
+async def on_message_jokes(bot, message):
+
+    words_in_message = [ word for word in re.split("\W", message.content.lower()) if word ] # We split the words in the message on any non word character (\W) and ignore empty words
+
+    await joke_je_suis(bot, message)
+    await joke_echec(message, words_in_message)
+    await joke_possible(message, words_in_message)
+    await joke_aled(message, words_in_message)
+    await cursed_words(message, words_in_message)
+    await marseillais_word(message, words_in_message)
