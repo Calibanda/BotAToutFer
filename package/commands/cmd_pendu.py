1dc92003cd6b30af3a634972769d90a300cb7356
Calibanda <clement@seijido.fr>
2020-11-17 11:35:47 +0100

    The hanging game is now independent by text channel

--- package/commands/cmd_pendu.py
+++ package/commands/cmd_pendu.py
@@ -8,130 +8,154 @@ from discord.ext import commands
 
 import const
 
+
 class Pendu(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
 
-        self.playing = False
-        self.secret_word = ""
-        self.visible_word = ""
-        self.number_stroke = 10
-        self.gessed_letters = []
+        self.current_games = {} # {discord.TextChannel: {secret_word: "", visible_word: "", number_stroke: 10, gessed_letters: []}}
+
 
     @commands.command(name="pendu", help="Joue au pendu")
     async def pendu(self, ctx, option: str="help"):
         option = option.casefold().strip()
 
-        if option=="status":
-            await self.display_status_message(ctx)
+        if option == "status":
+            await self.status(ctx)
 
-        elif option=="start" and not self.playing:
+        elif option == "start":
             await self.start(ctx)
 
-        elif option=="start":
-            response = f"Une partie est déjà en cours !\nIl reste {self.number_stroke} coup(s) et voici le mot à deviner : " + self.visible_word.replace("*", "\*")
-            await ctx.send(response)
+        elif option == "stop":
+            await self.stop(ctx)
 
-        elif option=="stop":
-            self.playing = False
-            response = f"Ohhh, dommage, mais je comprend que vous souhaitez arrêter. Voici le mot qui était à deviner : {self.secret_word}"
-            await ctx.send(response)
+        elif option == "restart" or option == "reboot":
+            await self.restart(ctx)
 
-        elif option=="restart" or option=="reboot":
-            if self.playing:
-                response = f"Bon, on recommence. Mais juste pour info, le mot précédent à deviner était : {self.secret_word}"
-                await ctx.send(response)
-
-            await self.start(ctx)
-
-        elif re.match(r"^[a-z]$", option) and self.playing:
-            response = ""
-            if option in self.gessed_letters:
-                response = f"Vous avez déjà demandé la lettre {option} !"
-
-            elif option in self.secret_word:
-                response = "Oui !"
-                self.gessed_letters.append(option)
-
-                for i in range(len(self.secret_word)):
-                    if self.secret_word[i] == option:
-                        self.visible_word = self.visible_word[:i] + option + self.visible_word[i + 1:]
-                if self.visible_word == self.secret_word: #WIN
-                    response += f" C'est gagné ! Vous avez deviné le mot {self.secret_word}."
-                    self.playing = False
-                    await ctx.send(response)
-                    return
-
-            else:
-                response = "Non !"
-                self.gessed_letters.append(f"~~{option}~~")
-                self.number_stroke -= 1
-
-                if self.number_stroke == 0: # LOSE
-                    response += " C'est perdu !"
-                    self.playing = False
-                    response += "\n```\n" + self.hanged_drawing() + "\n```\n"
-                    response += f"Le mot à deviner était : {self.secret_word}"
-                    await ctx.send(response)
-                    return
-
-            await ctx.send(response)
-            await self.display_status_message(ctx)
-
-        elif (option=="stop" or re.match(r"^[a-z]$", option)) and not self.playing:
-            response = "Aucune partie de pendu n'est en cours ! Mais vous pouvez en lancer une avec la commande '!pendu start'"
-            await ctx.send(response)
+        elif re.match(r"^[a-z]$", option):
+            await self.guess(ctx, option)
 
         else: # Display help
             response = "Les commandes du jeu du pendu :\n```\n!pendu start                       Lance une partie\n!pendu stop                        Arrête la partie en cours\n!pendu restart (ou !pendu reboot)  Arrête la partie en cours et en relance une\n!pendu status                      Affiche le status de la partie en cours\n!pendu <lettre>                    Proposer une lettre\n```"
             await ctx.send(response)
 
 
+    async def status(self, ctx):
+        if ctx.channel in self.current_games:
+            response = "Le mot à deviner : " + self.current_games[ctx.channel][visible_word].replace("*", "\*") + "\nLes lettres déjà proposées : "
+            for letter in self.current_games[ctx.channel][gessed_letters]:
+                response += f"{letter} "
+            response += "\nLe pendu :\n```\n" + self.hanged_drawing(self.current_games[ctx.channel][number_stroke]) + "\n```"
+            await ctx.send(response)
+        else:
+            await self.no_game(ctx)
+
+
     async def start(self, ctx):
-        self.playing = True
-        self.number_stroke = 10
-        self.gessed_letters = []
-        with open(const.SCRABBLE_DICTIONARY_PATH, "r", encoding="utf-8") as scrabble_disctionary:
-            self.secret_word = random.choice(scrabble_disctionary.readlines()).casefold().strip()
+        if ctx.channel not in self.current_games:
+            game = {}
 
-        self.visible_word = "*" * len(self.secret_word)
+            with open(const.SCRABBLE_DICTIONARY_PATH, "r", encoding="utf-8") as scrabble_disctionary:
+                game[secret_word] = random.choice(scrabble_disctionary.readlines()).casefold().strip()
 
-        response = f"Je lance une partie de pendu !\nVous avez {self.number_stroke} coup(s) pour trouver le mot secret selon les règles du pendu !\nVoici le mot à deviner : " + self.visible_word.replace("*", "\*")
+            game[visible_word] = "*" * len(self.current_games[ctx.channel][secret_word])
+            game[number_stroke] = 10
+            game[gessed_letters] = []
+
+            self.current_games[ctx.channel] = game
+
+            response = f"Je lance une partie de pendu !\nVous avez {game[number_stroke]} coup(s) pour trouver le mot secret selon les règles du pendu !\nVoici le mot à deviner : " + game[visible_word].replace("*", "\*")
+
+        else:
+            response = f"Une partie est déjà en cours !\nIl reste {self.current_games[ctx.channel][number_stroke]} coup(s) et voici le mot à deviner : " + self.current_games[ctx.channel][visible_word].replace("*", "\*")
+
         await ctx.send(response)
 
 
-    def hanged_drawing(self):
-        if self.number_stroke == 10:
+    async def stop(self, ctx):
+        if ctx.channel in self.current_games:
+            response = f"Ohhh, dommage, mais je comprend que vous souhaitez arrêter. Voici le mot qui était à deviner : {self.current_games[ctx.channel][secret_word]}"
+            del self.current_games[ctx.channel]
+            await ctx.send(response)
+        else:
+            await self.no_game(ctx)
+
+
+    async def no_game(self, ctx):
+        response = "Aucune partie de pendu n'est en cours ! Mais vous pouvez en lancer une avec la commande '!pendu start'"
+        await ctx.send(response)
+
+
+    async def restart(self, ctx):
+        if ctx.channel in self.current_games:
+            response = f"Bon, on recommence. Mais juste pour info, le mot précédent à deviner était : {self.current_games[ctx.channel][secret_word]}"
+            del self.current_games[ctx.channel]
+            await ctx.send(response)
+
+        await self.start(ctx)
+
+
+    async def guess(self, ctx, option):
+        if ctx.channel in self.current_games:
+            response = ""
+            if option in self.current_games[ctx.channel][gessed_letters]:
+                response = f"Vous avez déjà demandé la lettre {option} !"
+
+            elif option in self.current_games[ctx.channel][secret_word]:
+                response = "Oui !"
+                self.current_games[ctx.channel][gessed_letters].append(option)
+
+                for i in range(len(self.current_games[ctx.channel][secret_word])):
+                    if self.current_games[ctx.channel][secret_word][i] == option:
+                        self.current_games[ctx.channel][visible_word] = self.current_games[ctx.channel][visible_word][:i] + option + self.current_games[ctx.channel][visible_word][i + 1:]
+                if self.current_games[ctx.channel][visible_word] == self.current_games[ctx.channel][secret_word]: #WIN
+                    response += f" C'est gagné ! Vous avez deviné le mot {self.current_games[ctx.channel][secret_word]}."
+                    del self.current_games[ctx.channel]
+                    await ctx.send(response)
+                    return
+
+            else:
+                response = "Non !"
+                self.current_games[ctx.channel][gessed_letters].append(f"~~{option}~~")
+                self.current_games[ctx.channel][number_stroke] -= 1
+
+                if self.current_games[ctx.channel][number_stroke] == 0: # LOSE
+                    response += " C'est perdu !"
+                    del self.current_games[ctx.channel]
+                    response += "\n```\n" + self.hanged_drawing(self.current_games[ctx.channel][number_stroke]) + "\n```\n"
+                    response += f"Le mot à deviner était : {self.current_games[ctx.channel][secret_word]}"
+                    await ctx.send(response)
+                    return
+
+            await ctx.send(response)
+            await self.status(ctx)
+
+        else:
+            await self.no_game(ctx)
+
+
+    def hanged_drawing(self, number_stroke):
+        if number_stroke == 10:
             return ""
-        elif self.number_stroke == 9:
+        elif number_stroke == 9:
             return "___"
-        elif self.number_stroke == 8:
+        elif number_stroke == 8:
             return " |\n |\n |\n |\n |\n_|_"
-        elif self.number_stroke == 7:
+        elif number_stroke == 7:
             return " ____________\n |\n |\n |\n |\n |\n_|_"
-        elif self.number_stroke == 6:
+        elif number_stroke == 6:
             return " ____________\n |/\n |\n |\n |\n |\n_|_"
-        elif self.number_stroke == 5:
+        elif number_stroke == 5:
             return " ____________\n |/     |\n |\n |\n |\n |\n_|_"
-        elif self.number_stroke == 4:
+        elif number_stroke == 4:
             return " ____________\n |/     |\n |      O\n |\n |\n |\n_|_"
-        elif self.number_stroke == 3:
+        elif number_stroke == 3:
             return " ____________\n |/     |\n |      O\n |      |\n |      |\n |\n_|_"
-        elif self.number_stroke == 2:
+        elif number_stroke == 2:
             return " ____________\n |/     |\n |      O\n |     _|_\n |      |\n |\n_|_"
-        elif self.number_stroke == 1:
+        elif number_stroke == 1:
             return " ____________\n |/     |\n |      O\n |     _|_\n |      |\n |     /\n_|_"
-        elif self.number_stroke == 0:
+        elif number_stroke == 0:
             return " ____________\n |/     |\n |      O\n |     _|_\n |      |\n |     / \\ \n_|_"
 
-
-    async def display_status_message(self, ctx):
-        if self.playing:
-            response = "Le mot à deviner : " + self.visible_word.replace("*", "\*") + "\nLes lettres déjà proposées : "
-            for letter in self.gessed_letters:
-                response += f"{letter} "
-            response += "\nLe pendu :\n```\n" + self.hanged_drawing() + "\n```"
-        else:
-            response = "Aucune partie en cours !"
-        await ctx.send(response)
