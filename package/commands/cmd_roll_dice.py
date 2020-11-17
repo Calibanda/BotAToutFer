5d2d63b0322673fabfbb6fab45e13d254bf4a570
Calibanda <clement@seijido.fr>
2020-11-17 09:33:47 +0100

    Protect the command from an empty string and a too high amount of dice

--- package/commands/cmd_roll_dice.py
+++ package/commands/cmd_roll_dice.py
@@ -9,13 +9,17 @@ class Roll_dice(commands.Cog):
         self._last_member = None
 
     @commands.command(name="roll", help="Simule un lancer de dés au format xDx")
-    async def roll(self, ctx, dice: str):
+    async def roll(self, ctx, dice: str=""):
         """Rolls a dice in xDx format."""
         try:
             number_of_dice, number_of_sides = map(int, dice.lower().split('d'))
-            dices = [ random.randint(1, number_of_sides) for r in range(number_of_dice) ]
-            total = sum(dices)
-            response = f"**{total}** (" + " + ".join(str(d) for d in dices) + f" = {total})"
+            if number_of_dice <= 200 and number_of_sides <= 200:
+                dices = [ random.randint(1, number_of_sides) for r in range(number_of_dice) ]
+                total = sum(dices)
+                response = f"**{total}** (" + " + ".join(str(d) for d in dices) + f" = {total})"
+            else:
+                response = "Ohh la flemme de lancer tous ces dés !"
+            
             await ctx.send(response)
         except Exception:
             await ctx.send('Format has to be in xDx!')
