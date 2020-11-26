f57f723802703801a51e8057a5b7ad5b22585bb4
Calibanda <clement@seijido.fr>
2020-11-26 20:41:32 +0100

    Add the command crepes

--- package/commands/cmd_discussion.py
+++ package/commands/cmd_discussion.py
@@ -1,5 +1,6 @@
 # siscussion commands for BotAToutFer
 import datetime
+import random
 
 from discord.ext import commands
 
@@ -34,4 +35,27 @@ class Discussion(commands.Cog):
         await ctx.send(response)
 
         response = f"Un peu de patience {ctx.author.mention}, je sais que tu veux tes cadeaux petit vénal :gift:"
+        await ctx.send(response)
+
+    @commands.command(name="crepes", help="Et pas galette hein ?")
+    async def crepes(self, ctx):
+        response = (
+              f"Recette des crêpes pour 4 personnes :\n"
+            + f"- 3 œufs\n"
+            + f"- {random.randint(300, 500)} g de beurre fondu\n"
+            + f"- 50 cl de lait\n"
+            + f"- {random.randint(300, 500)} g de beurre fondu\n"
+            + f"- 500 ml (ou 250 g) de farine\n"
+            + f"- {random.randint(300, 500)} g de beurre fondu\n"
+            + f"- 1 sachet de sucre vanillé\n"
+            + f"- {random.randint(300, 500)} g de beurre fondu\n"
+            + f"1/ Dans un grand récipient, mélangez les œufs, le lait ainsi que la farine.\n"
+            + f"2/ Ajoutez le beurre fondu.\n"
+            + f"3/ Ajoutez le sucre vanillé.\n"
+            + f"4/ Ajoutez le beurre fondu.\n"
+            + f"5/ Finissez en mélangeant le tout.\n"
+            + f"6/ Ajoutez le beurre fondu.\n"
+            + f"7/ Huilez une crêpière très chaude et formez des crêpes à l'aide d'une petite louche.\n"
+            + f"8/ Ajoutez le beurre fondu.\n"
+        )
         await ctx.send(response)
