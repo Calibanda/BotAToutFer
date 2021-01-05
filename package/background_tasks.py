4f0b3583d18ff2959ac3053278e1ecb6304e4fdb
Calibanda <clement.seijido@protonmail.com>
2021-01-05 13:03:09 +0100

    Reduce chance to send a cat or a ref fox picture in chat

--- package/background_tasks.py
+++ package/background_tasks.py
@@ -20,7 +20,7 @@ class Tasks(commands.Cog):
     @tasks.loop(minutes=6.0)
     async def cat(self):
         for channel in self.bot.autorized_channel:
-            if secrets.randbelow(160) < 1 and datetime.datetime.now().hour in range(7, 23): # Statisticly send 1 message per day (one chance on 160 every 6 minutes between 7AM and 11PM)
+            if secrets.randbelow(200) < 1 and datetime.datetime.now().hour in range(7, 23): # Statisticly send 1 message per day (one chance on 160 every 6 minutes between 7AM and 11PM)
                 try:
                     async with aiohttp.ClientSession() as session:
 
