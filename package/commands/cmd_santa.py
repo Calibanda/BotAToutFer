e39d34885be28860c605a2a43ceeee95f4256f86
Calibanda <clement.seijido@protonmail.com>
2021-01-14 15:48:17 +0100

    Improve math calculation of the secrets santas

--- package/commands/cmd_santa.py
+++ package/commands/cmd_santa.py
@@ -22,26 +22,13 @@ class Santa(commands.Cog):
             await ctx.send(response)
             return
 
-        christmas_is_ready = False
+        senders = list(users)
+        random.shuffle(senders)
+        recipients = senders.copy()
+        recipients.append(recipients[0])
+        del recipients[0]
 
-        while not christmas_is_ready:
-            senders = list(users)
-            recipients = list(users)
-            list_couples = []
-
-            for sender in senders:
-                recipient = random.choice(recipients)
-                recipients.remove(recipient)
-
-                list_couples.append((sender, recipient))
-            
-            christmas_is_ready = True
-
-            for couple in list_couples:
-                if couple[0] == couple[1]:
-                    christmas_is_ready = False
-
-        for sender, recipient in list_couples:
+        for sender, recipient in zip(senders, recipients):
             dm_channel = sender.dm_channel if sender.dm_channel else await sender.create_dm()
             message = f"Salut {sender.name} ! Le tirage au sort des Pères Noëls secrets à été effectué :santa: \nIl ne te reste plus qu'à trouver un cadeau pour {recipient} :gift_heart:. Et un joyeux Noël à tous ! "
             await dm_channel.send(message)
