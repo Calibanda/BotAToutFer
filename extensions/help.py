6d051a5d7636d8e37d46c8f3555ebf9df531c609
Calibanda <clement.seijido@protonmail.com>
2021-01-18 17:48:18 +0100

    Now separate help message if message is too long

--- extensions/help.py
+++ extensions/help.py
@@ -15,19 +15,23 @@ class Help(commands.Cog):
 
     @commands.command(name="help", help="Affiche ce message", pass_context=True)
     async def help(self, ctx, command=""):
-        response = f"```\n{self.bot.description}\n\n"
+        response = f"```\n{self.bot.description}\n\n" # We create a new response string
 
         list_cogs = sorted([ cog for cog in self.bot.cogs if not hasattr(self.bot.get_cog(cog), "hidden_cog") or not self.bot.get_cog(cog).hidden_cog ])
         number_spaces = len(sorted([c.name for c in self.bot.commands], key=lambda command: len(command))[-1]) + 1
 
         for cog in list_cogs:
-            response += f"{cog} :\n"
-
+            cog_help = f"{cog} :\n" # We create the new cog hepl string
             for command in sorted(self.bot.get_cog(cog).get_commands(), key=lambda command: command.name):
-                response += "  " + command.name.ljust(number_spaces, " ") + f"{command.help}\n"
+                cog_help += "  " + command.name.ljust(number_spaces, " ") + f"{command.help}\n"
 
-            response += "\n"
-
-        response += "```"
-
-        await ctx.send(response)
+            if len(response + cog_help) + 3 < 2000: # If next help can be added without exeed the Discord limit
+                response += cog_help # We add the cog help to the response
+            else: # If cog help CANNOT be added without exeed the Discord limit
+                response += "```"
+                await ctx.send(response) # We send the response
+                response = "```\n" # We create a new response
+                response += cog_help # We add the cog help to the new response
+        if len(response) > 3:# We exhaust all the cogs, we send the cog help if any
+            response += "```"
+            await ctx.send(response) 
