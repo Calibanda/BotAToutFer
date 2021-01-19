3ea71122886d044c36533ebf352d3e994a4bc467
Calibanda <clement.seijido@protonmail.com>
2021-01-19 13:25:49 +0100

    Lightly icrease chance to send a cute picture

--- extensions/pictures.py
+++ extensions/pictures.py
@@ -26,7 +26,7 @@ class Pictures(commands.Cog):
     @tasks.loop(minutes=6.0)
     async def cat(self):
         for channel in self.bot.autorized_channels:
-            if random.randrange(200) < 1 and datetime.datetime.now().hour in range(7, 23): # Statisticly send 1 message per day (one chance on 160 every 6 minutes between 7AM and 11PM)
+            if random.randrange(180) < 1 and datetime.datetime.now().hour in range(7, 23): # Statisticly send 1 message per day (one chance on 160 every 6 minutes between 7AM and 11PM)
                 try:
                     async with aiohttp.ClientSession() as session:
 
