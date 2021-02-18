2c56281ee6404ee9d9d43e4a73ccbda09018dafd
Calibanda <clement.seijido@protonmail.com>
2021-02-18 23:04:59 +0100

    Remove useless line return

--- extensions/anniv.py
+++ extensions/anniv.py
@@ -25,8 +25,7 @@ class Anniversaire(commands.Cog):
 
     @commands.command(
         name="anniv",
-        help="Affiche l'anniversaire d'un utilisateur humain"
-        )
+        help="Affiche l'anniversaire d'un utilisateur humain")
     async def anniv(self, ctx, user: discord.User):
         if user:
             try:
@@ -48,8 +47,7 @@ class Anniversaire(commands.Cog):
     @commands.command(
         name="mon_anniv",
         help="Permet d'enregistrer sa date de naissance \
-        sous la forme DD/MM/YYYY"
-        )
+        sous la forme DD/MM/YYYY")
     async def mon_anniv(self, ctx, date: str=""):
         date = re.search(r"\d{2}\/\d{2}\/\d{4}", date)[0]
         if date:
