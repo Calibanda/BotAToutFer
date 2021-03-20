bcbd1d2ee818966dc4562ad51ebf71f444d7c184
Calibanda <clement.seijido@protonmail.com>
2021-03-20 13:55:22 +0100

    Add commands to add and remove on words jokes

--- extensions/message_jokes.py
+++ extensions/message_jokes.py
@@ -14,7 +14,6 @@ def setup(bot):
 class Jokes(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self._last_member = None
         self.hidden_cog = True
         self.LIST_JE_SUIS = [
             "je suis ",
@@ -47,6 +46,18 @@ class Jokes(commands.Cog):
             {"curse_word": "cock", "traduction": "cork"},
             {"curse_word": "dick", "traduction": "deck"},
         ]
+        self.JOKE_WORDS_PATH = os.path.join(
+            self.bot.SCRIPT_DIR,
+            "package",
+            "joke_words.json"
+        )
+
+        try:
+            with open(self.JOKE_WORDS_PATH, "r") as f:
+                self.joke_words = json.load(f)
+        except (FileNotFoundError, json.decoder.JSONDecodeError) as e:
+            self.bot.log.error(f"Caught exception:", exc_info=e)
+            self.joke_words = {}
 
     @commands.Cog.listener('on_message')
     async def message_jokes(self, message):
@@ -62,16 +73,11 @@ class Jokes(commands.Cog):
                 words_in_message.append(word)
 
         await self.joke_je_suis(message)
-        await self.joke_echec(message, words_in_message)
-        await self.joke_possible(message, words_in_message)
-        await self.joke_aled(message, words_in_message)
         await self.cursed_words(message, words_in_message)
-        await self.mousse(message, words_in_message)
-        await self.projet(message, words_in_message)
-        await self.respect(message, words_in_message)
-        await self.what(message, words_in_message)
-        await self.rip(message, words_in_message)
-        await self.alerte(message, words_in_message)
+
+        for key, value in self.joke_words.items():
+            if key in words_in_message:
+                await message.channel.send(value)
 
     async def joke_je_suis(self, message):
         has_suis = False
@@ -93,27 +99,6 @@ class Jokes(commands.Cog):
                 flags=re.IGNORECASE
             )[-1]
             response = f"Salut *{i_am}*, moi c'est le {self.bot.user.mention}"
-            await message.channel.send(response)
-
-    async def joke_echec(self, message, words_in_message):
-        if "échec" in words_in_message or "echec" in words_in_message:
-            # If the words "échec" or "echec" in the original message
-            response = "https://tenor.com/bq4o5.gif"
-            await message.channel.send(response)
-
-    async def joke_possible(self, message, words_in_message):
-        if "possible" in words_in_message or "impossible" in words_in_message:
-            # If the words "possible" or "impossible" in the original message
-            response = "https://tenor.com/XiKZ.gif"
-            await message.channel.send(response)
-
-    async def joke_aled(self, message, words_in_message):
-        if "aled" in words_in_message:
-            # If the word "aled" in the original message
-            response = (
-                "https://assets.classicfm.com/2017/23/aled-jones-new-"
-                + "1497019550-editorial-long-form-1.png"
-            )
             await message.channel.send(response)
 
     async def cursed_words(self, message, words_in_message):
@@ -139,53 +124,23 @@ class Jokes(commands.Cog):
             await message.channel.send(response)
             await message.delete()
 
-    async def mousse(self, message, words_in_message):
-        if "attention" in words_in_message or "mousse" in words_in_message:
-            # If the words "attention" or "mousse" in the original message
-            response = (  # Send the "ATTENTION À LA MOUSSE !!" gif
-                "https://media1.tenor.com/images/"
-                + "ef6a3c5ba7cbdc4b0140d7363dbc841f/tenor.gif"
-            )
-            await message.channel.send(response)
+    @commands.command(name="add-joke")
+    @commands.is_owner()
+    async def add_joke(self, ctx, trigger_word, gif_url):
+        self.joke_words[trigger_word] = gif_url
+        with open(self.JOKE_WORDS_PATH, "w", encoding="utf8") as f:
+            json.dump(self.joke_words, f, indent=4, ensure_ascii=False)
 
-    async def projet(self, message, words_in_message):
-        if "projet" in words_in_message:
-            # If the words "projet" in the original message
-            response = "https://tenor.com/RGiO.gif"
-            # Send the "CAR C'EST NOTRE PROJEEEEET !!" gif
-            await message.channel.send(response)
+        await ctx.send(gif_url)
 
-    async def respect(self, message, words_in_message):
-        if "respecter" in words_in_message or "respecte" in words_in_message:
-            # If the words "respecter" or "respecte" in the original message
-            response = (  # Send the "Yes master" gif
-                "https://tenor.com/view/master-spongebob-bowing-bow-yes-"
-                + "master-gif-5594685"
-            )
-            await message.channel.send(response)
-
-    async def what(self, message, words_in_message):
-        if "what" in words_in_message:
-            # If the word "what" in the original message
-            response = (  # Send a "WHHAAAATTT?!" minion gif
-                "https://tenor.com/view/minion-what-huh-gif-9361819"
-            )
-            await message.channel.send(response)
-
-    async def rip(self, message, words_in_message):
-        if "rip" in words_in_message:
-            # If the word "rip" in the original message
-            response = (  # Send a "RIP" gif
-                "https://tenor.com/view/rip-coffin-black-ghana-"
-                + "celebrating-gif-16743302"
-            )
-            await message.channel.send(response)
-
-    async def alerte(self, message, words_in_message):
-        if "alerte" in words_in_message:
-            # If the word "alerte" in the original message
-            response = (  # Send a "ALERTE GÉNÉRALE !" gif
-                "https://tenor.com/view/shout-yell-alerte-generale-alert-"
-                + "alerted-general-gif-17332933"
-            )
-            await message.channel.send(response)
+    @commands.command(name="remove-joke")
+    @commands.is_owner()
+    async def remove_joke(self, ctx, trigger_word):
+        try:
+            del self.joke_words[trigger_word]
+        except KeyError as e:
+            await ctx.send("This trigger word does not exist !")
+        else:
+            with open(self.JOKE_WORDS_PATH, "w", encoding="utf8") as f:
+                json.dump(self.joke_words, f, indent=4, ensure_ascii=False)
+            await ctx.send(f"{trigger_word} deleted.")
