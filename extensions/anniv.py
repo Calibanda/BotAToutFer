85f71fd028af907d573dff209e3ab4f392a40650
Calibanda <clement.seijido@protonmail.com>
2021-12-06 12:20:57 +0100

    Implement tasks.loop to create a birthday wish. Update json creation to include server_id. Add date parser.

--- extensions/anniv.py
+++ extensions/anniv.py
@@ -1,11 +1,18 @@
-# anniv commands for BotÀToutFer
+"""Anniversaire Cog for the "BotAToutFer" discord bot
+
+(C) 2021 Clément SEIJIDO
+Released under GNU General Public License v3.0 (GNU GPLv3)
+e-mail clement@seijido.fr
+"""
+
 import os
 import datetime
 import json
-import re
+import asyncio
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+import dateutil.parser as parser
 
 
 def setup(bot):
@@ -15,7 +22,6 @@ def setup(bot):
 class Anniversaire(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self._last_member = None
         # Path of the json file containing the users birthdays
         self.BIRTHDAYS_PATH = os.path.join(
             self.bot.SCRIPT_DIR,
@@ -23,41 +29,103 @@ class Anniversaire(commands.Cog):
             "birthdays.json"
         )
 
-    @commands.command(name="anniv", help="Affiche l'anniversaire d'un utilisateur humain")
+        self.birthday_loop.start()
+
+    def cog_unload(self):
+        self.birthday_loop.cancel()
+
+    @tasks.loop(hours=24)
+    async def birthday_loop(self):
+        try:
+            with open(self.BIRTHDAYS_PATH, "r") as f:
+                birthdays = json.load(f)
+        except (FileNotFoundError, json.decoder.JSONDecodeError) as e:
+            self.bot.log.error(
+                "birthdays.json file not found: ",
+                exc_info=e
+            )
+            birthdays = {}
+
+        now = datetime.datetime.now()
+        today = f"{now.day:02d}/{now.month:02d}"  # DD/MM
+
+        for user in birthdays:
+            if birthdays[user]["birthdate"][:5] == today:
+                message = f"Bon anniversaire " \
+                          f"{self.bot.get_user(user).mention} ! :tada:"
+                server = self.bot.get_guild(birthdays[user]["server_id"])
+                await server.system_channel.send(message)
+
+    @birthday_loop.before_loop
+    async def before_birthday_loop(self):
+        await self.bot.wait_until_ready()
+
+        alarm = datetime.datetime.now().replace(
+            hour=9,
+            minute=0,
+            second=0,
+            microsecond=0
+        )
+        delta = (alarm - datetime.datetime.now()).total_seconds()
+        if delta < 0:  # 9AM is passed today
+            delta += datetime.timedelta(days=1).total_seconds()
+
+        await asyncio.sleep(delta)  # await next 9AM
+
+    @commands.command(
+        name="anniv",
+        help="Affiche l'anniversaire d'un utilisateur humain"
+    )
     async def anniv(self, ctx, user: discord.User):
         if user:
             try:
                 with open(self.BIRTHDAYS_PATH, "r") as f:
                     birthdays = json.load(f)
             except (FileNotFoundError, json.decoder.JSONDecodeError) as e:
-                self.bot.log.error("birthdays.json file not found: ", exc_info=e)
+                self.bot.log.error(
+                    "birthdays.json file not found: ",
+                    exc_info=e
+                )
                 birthdays = {}
 
             if str(user.id) in birthdays:
-                response = f"L'anniv de cette chouette personne est le {birthdays[str(user.id)][:-5]}"
+                date = birthdays[str(user.id)]["birthdate"][:5]  # DD/MM
+                response = f"L'anniv de cette chouette personne est le {date}"
             else:
                 response = f"Jamais entendu parler de cette personne"
         else:
             response = f"Jamais entendu parler de cette personne"
         await ctx.send(response)
 
-    @commands.command(name="mon-anniv", help="Permet d'enregistrer sa date de naissance sous la forme DD/MM/YYYY")
-    async def mon_anniv(self, ctx, date: str=""):
-        date = re.search(r"\d{2}\/\d{2}\/\d{4}", date)[0]
-        if date:
-            try:
-                with open(self.BIRTHDAYS_PATH, "r") as f:
-                    birthdays = json.load(f)
-            except (FileNotFoundError, json.decoder.JSONDecodeError) as e:
-                self.bot.log.error("birthdays.json file not found: ", exc_info=e)
-                birthdays = {}
-
-            birthdays[ctx.author.id] = date
-
-            with open(self.BIRTHDAYS_PATH, "w") as f:
-                json.dump(birthdays, f, indent=4)
-
-            response = "C'est bon ! Je me souviendrai de ta date de naissance !"
-        else:
+    @commands.command(
+        name="mon-anniv",
+        help="Permet d'enregistrer sa date de naissance"
+    )
+    async def mon_anniv(self, ctx, date: str = ""):
+        try:
+            date = parser.parse(date)
+        except parser.ParserError as e:
             response = "Euhhh, c'est quoi cette date bourrée ?"
+            await ctx.send(response)
+            return
+
+        try:
+            with open(self.BIRTHDAYS_PATH, "r") as f:
+                birthdays = json.load(f)
+        except (FileNotFoundError, json.decoder.JSONDecodeError) as e:
+            self.bot.log.error(
+                "birthdays.json file not found: ",
+                exc_info=e
+            )
+            birthdays = {}
+
+        birthdays[str(ctx.author.id)] = {
+            "server_id": str(ctx.guild.id),
+            "birthdate": f"{date.day:02d}/{date.month:02d}/{date.year}"
+        }
+
+        with open(self.BIRTHDAYS_PATH, "w") as f:
+            json.dump(birthdays, f, indent=4)
+
+        response = "C'est bon ! Je me souviendrai de ta date de naissance !"
         await ctx.send(response)
